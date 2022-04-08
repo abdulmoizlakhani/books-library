3eb6a3f8d633cca43de3b8cfdd25c88479c2548a
Abdul Moiz Lakhani <lakhaniabdulmoiz@gmail.com>
2022-04-08 16:24:02 +0500

    - initial client react app setup

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+// components
+import BookList from "./components/BookList";
+
+function App() {
+  return (
+    <div className="main">
+      <h1>Books Library</h1>
+      <BookList />
+    </div>
+  );
+}
+
+export default App;
